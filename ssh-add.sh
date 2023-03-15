a43718ae4ebe29bb4bcdf1acaeb5f2f65bb325fc
Павел Безансон <bznpavel@yandex.ru>
2023-03-15 10:05:56 +0300

    Fixed the problem of running the script

--- ssh-add.sh
+++ ssh-add.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
+[ -n "$SSH_AUTH_SOCK" ] || eval $(ssh-agent)
 ssh-add ~/.ssh/hite
+
+# about problem
+# https://superuser.com/questions/1535186/why-are-ssh-agent-and-ssh-add-not-working-together-in-one-bash-script
