770e1ef204776869116b23e1b1820243713a98de
Павел Безансон <bznpavel@yandex.ru>
2023-03-17 16:24:22 +0300

    Made OPC projects

---
+++ OpcServer/OpcServer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  OpcServer.py
+#  Author: Poul Bezanson
+
+def main(args):
+	return 0
+
+if __name__ == '__main__':
+    print("Hellow word")
+    
+    
+    
+    import sys
+    sys.exit(main(sys.argv))
